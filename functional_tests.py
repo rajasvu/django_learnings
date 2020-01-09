1828e28d0b4ad57985fadaf87358587b53723a05
Kumar Raja Gattamaneni <38221186+gattamaneni@users.noreply.github.com>
2020-01-09 21:36:34 +0530

    Update functional_tests.py
    Updated functional test to verify ability of user entering a to-do item in the webpage

--- functional_tests.py
+++ functional_tests.py
@@ -2,7 +2,10 @@
 First functional test to verify the environment setup for django development
 """
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
@@ -18,13 +21,20 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         # She is invited to enter a to-do item straight away
-
+        inputbox = self.browser.find_element_by_id('todo_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
-
+        inputbox.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         # "1: Buy peacock feathers" as an item in a to-do list
+        table = self.browser.find_element_by_id('todo_list_table')
+        todo_records = self.browser.find_elements_by_tag_name('tr')
+        self.assertEqual(any(todo_record.text == '1: Buy peacock feathers' for todo_record in todo_records))
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
         # The page updates again, and now shows both items on her list
@@ -32,6 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # Then she sees that the site has generated a unique URL for her -- there is some explanatory text to that effect.
         # She visits that URL - her to-do list is still there.
         # Satisfied, she goes back to sleep
+        self.fail('Finish the test')
 
 
 if __name__ == '__main__':
