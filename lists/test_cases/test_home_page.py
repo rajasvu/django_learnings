6bf5a5bc39e55b5f3308c2328f20b6b18a72f1db
Gattamaneni Kumar Raja <rajasvu@gmail.com>
2022-11-01 16:12:03 +0000

    Removing old files

--- lists/test_cases/test_home_page.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class HomePageTest(TestCase):
-    def test_home_page_uses_correct_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
